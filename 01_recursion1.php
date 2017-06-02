71299caaa4c6d769b0a8330e43aed8ab45191bb6
Evgeny Lysenko <lisenkoevg@gmail.com>
2017-06-02 08:40:14 +0300

    Сумма элементов одномерного массива с рекурсией

--- 01_recursion1.php
+++ 01_recursion1.php
@@ -6,7 +6,8 @@ $arr = [1, 1, 1, 1];
 function sum_rec($arr, $n = 0) {
   $sum = 0;
   if (array_key_exists($n, $arr)) {
-    $sum = $arr[$n] + sum_rec($arr, $n + 1);
+    $sum = $arr[$n];
+    $sum += sum_rec($arr, $n + 1);
   }
   return $sum;
 }
